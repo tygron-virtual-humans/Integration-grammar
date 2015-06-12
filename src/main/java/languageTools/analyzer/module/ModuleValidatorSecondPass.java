1fb6fcdbca3544e6129c8077d9740e5b262f1c75
RolfStarre <rolf.starre@gmail.com>
2015-06-12 11:44:11 +0200

    emotion

--- src/main/java/languageTools/analyzer/module/ModuleValidatorSecondPass.java
+++ src/main/java/languageTools/analyzer/module/ModuleValidatorSecondPass.java
@@ -62,6 +62,7 @@ public class ModuleValidatorSecondPass {
 	private final static String AGENT = "agent/1";
 	private final static String ME = "me/1";
 	private final static String PERCEPT = "percept/1";
+	private final static String EMOTION = "emotion/1";
 	private final static String EMOTION2 = "emotion/2";
 	private final static String EMOTION3 = "emotion/3";
 	private final static String RECEIVED = "received/2";
@@ -244,6 +245,7 @@ public class ModuleValidatorSecondPass {
 		reserved.add(AGENT);
 		reserved.add(ME);
 		reserved.add(PERCEPT);
+		reserved.add(EMOTION);
 		reserved.add(EMOTION2);
 		reserved.add(EMOTION3);
 		reserved.add(RECEIVED);
--- src/main/java/languageTools/analyzer/test/TestValidatorSecondPass.java
+++ src/main/java/languageTools/analyzer/test/TestValidatorSecondPass.java
@@ -54,6 +54,7 @@ public class TestValidatorSecondPass {
 	private final static String AGENT = "agent/1";
 	private final static String ME = "me/1";
 	private final static String PERCEPT = "percept/1";
+	private final static String EMOTION = "emotion/1";
 	private final static String EMOTION2 = "emotion/2";
 	private final static String EMOTION3 = "emotion/3";
 	private final static String RECEIVED = "received/2";
@@ -184,6 +185,7 @@ public class TestValidatorSecondPass {
 		reserved.add(AGENT);
 		reserved.add(ME);
 		reserved.add(PERCEPT);
+		reserved.add(EMOTION);
 		reserved.add(EMOTION2);
 		reserved.add(EMOTION3);
 		reserved.add(RECEIVED);
